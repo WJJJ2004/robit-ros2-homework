7ffdfb1c2397c1aab76d029e591b91df5d22b478
WJJJ2004 <dldnjswns7412@gmail.com>
2024-10-13 22:04:10 +0900

    2nd push

--- HW2/my_publisher/src/publisher_node.cpp
+++ HW2/my_publisher/src/publisher_node.cpp
@@ -10,14 +10,17 @@ public:
         timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&MyPublisher::publish_message, this));   // 1초마다 publish_message() 함수를 호출하는 타이머 설정
     }
 
+    // 1초마다 publish_message() 함수를 호출하는 타이머 설정
+    // create_wall_timer는 주기적으로 콜백 함수를 호출하는 타이머를 생성함
+
 private:
     void publish_message() 
     {
         auto message = my_custom_interfaces::msg::MyCustomMessage();
 
         std::cout << "Enter a message to publish: ";
-        std::getline(std::cin, message.data);        
-        publisher_->publish(message); // 퍼블리셔를 사용하여 메시지 발행
+        std::getline(std::cin, message.data);        // 사용자에게서 메서지를 입력받음 
+        publisher_->publish(message); // 퍼블리셔 메소드를 사용하여 메시지 발행
     }
     
     rclcpp::Publisher<my_custom_interfaces::msg::MyCustomMessage>::SharedPtr publisher_;
@@ -27,7 +30,7 @@ private:
 int main(int argc, char *argv[]) 
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<MyPublisher>());
+    rclcpp::spin(std::make_shared<MyPublisher>());  // MyPublisher 노드를 실행하고, 콜백이 발생할 때까지 대기
     rclcpp::shutdown();
     return 0;
 }
--- HW2/my_subscriber/src/subscriber_node.cpp
+++ HW2/my_subscriber/src/subscriber_node.cpp
@@ -4,10 +4,14 @@
 class MySubscriber : public rclcpp::Node 
 {
 public:
-    MySubscriber() : Node("my_subscriber") 
+    MySubscriber() : Node("my_subscriber") // 구독자 노스 생성 및 콜백함수 세팅하기 
     {
         subscription_ = this->create_subscription<my_custom_interfaces::msg::MyCustomMessage>("custom_topic", 10, std::bind(&MySubscriber::receive_message, this, std::placeholders::_1));
     }
+
+    // create_subscription 함수를 사용하여 구독자를 생성
+    // "custom_topic" 토픽을 구독하며, 큐 크기는 10
+    // 메시지를 수신할 때마다 receive_message 콜백 함수가 호출됨
 
 private:
     void receive_message(const my_custom_interfaces::msg::MyCustomMessage::SharedPtr msg) 
@@ -15,14 +19,14 @@ private:
         RCLCPP_INFO(this->get_logger(), "Received: '%s'", msg->data.c_str());
     }
     
-    rclcpp::Subscription<my_custom_interfaces::msg::MyCustomMessage>::SharedPtr subscription_;
+    rclcpp::Subscription<my_custom_interfaces::msg::MyCustomMessage>::SharedPtr subscription_;  // create_subscription을 저장할 구독자 변수를 선언함
 };
 
 int main(int argc, char *argv[]) 
 {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<MySubscriber>()); 
-    rclcpp::shutdown();
+    rclcpp::init(argc, argv); //시스템 초기화
+    rclcpp::spin(std::make_shared<MySubscriber>()); // MySubscriber 노드를 실행하고, 콜백이 발생할 때까지 기다림
+    rclcpp::shutdown(); // 시스템 종료
     return 0;
 }
 
